bf5b210ba32f759a8dbe63bf4f0d50cbf347544b
Marchopkins96 <>
2023-12-23 16:42:00 +0000

    Add function to generate random word from list

--- run.py
+++ run.py
@@ -1,3 +1,15 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+import random 
+
+def get_random_words_from_wordlist():
+    """
+    Picks the random word from the 
+    wordlist.txt file for the game
+    """
+    wordlist = []
+
+    with open("wordlist.txt", 'r') as file:
+        wordlist = file.read().split("\n")
+
+    word = random.choice(wordlist)
+
+    return work
