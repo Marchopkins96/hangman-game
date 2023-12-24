09d0bbf7077a0c132ed966c4434dff66af960bcd
Marchopkins96 <>
2023-12-24 16:59:48 +0000

    Add function to display hangman game

--- run.py
+++ run.py
@@ -79,3 +79,21 @@ def get_random_word_list():
 
     return word
 
+def displayBoard(hang, missedLetters, correctLetters, secretWord):
+    print(hang[len(missedLetters)])
+    print()
+
+    print('Missed Letters:', end=' ')
+    for letter in missedLetters:
+        print(letter, end=' ')
+    print("\n")
+
+    blanks = '_' * len(secretWord)
+
+    for i in range(len(secretWord)):   #replace blanks with correctly guessed letter
+        if secretWord[i] in correctLetters:
+            blanks = blanks[:i] + secretWord[i] + blanks[i+1:]
+
+    for letter in blanks:  #show the secret word with spaces in between each letter
+        print(letter, end=' ')
+
