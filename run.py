a91011fe0d3919ce958c8592d81356ca789d87b3
Marchopkins96 <>
2023-12-27 14:43:57 +0000

    Add Function for user to play game with option to play again

--- run.py
+++ run.py
@@ -97,7 +97,7 @@ def displayBoard(hang, missedLetters, correctLetters, secretWord):
     for letter in blanks:  #show the secret word with spaces in between each letter
         print(letter, end=' ')
 
-def get_guess(alreadyGuessed):
+def getGuess(alreadyGuessed):
     while True:
         guess = input('Guess a letter: ')
         guess = guess.lower()
@@ -109,4 +109,51 @@ def get_guess(alreadyGuessed):
             print('Please enter a letter.')
         else:
             return guess
+
+def playAgain():
+    return input("\nDo you want to play again? ").lower().startswith('y')
+
+missedLetters = ''
+correctLetters = ''
+secretWord = get_random_word_list()
+gameIsDone = False
+
+while True:
+    displayBoard(hang, missedLetters, correctLetters, secretWord)
+
+    guess = getGuess(missedLetters + correctLetters)
+
+    if guess in secretWord:
+        correctLetters = correctLetters + guess
+
+        foundAllLetters = True
+        for i in range(len(secretWord)):
+            if secretWord[i] not in correctLetters:
+                foundAllLetters = False
+                break
+
+        if foundAllLetters:
+            print('\nYes! The secret word is "' +
+            secretWord + '"! You have won!')
+            gameIsDone = True
+    
+    else:
+        missedLetters = missedLetters + guess
+
+        if len(missedLetters) == len(hang) - 1:
+            displayBoard(hang, missedLetters, 
+            correctLetters, secretWord)
+            print('You have run out of guesses!\nAfter ' + str(len(missedLetters)) + ' missed guesses and ' 
+            + str(len(correctLetters)) + ' correct guesses, the word was "'
+            + secretWord + '"')
+            gameIsDone = True
+
+    if gameIsDone:
+        if playAgain():
+            missedLetters = ''
+            correctLetters = ''
+            gameIsDone = False
+            secretWord = get_random_word_list()
+        else: 
+            break
 
