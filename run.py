33f5664ee611b852b296f3208ae423f8900e5bfe
Marchopkins96 <>
2023-12-28 20:15:24 +0000

    Add function to display some dashes and letters for user word guess

--- run.py
+++ run.py
@@ -9,3 +9,21 @@ def get_random_word_from_wordlist():
         wordlist = file.read().split('\n')
     word = random.choice(wordlist)
     return word
+
+def get_some_letters(word):
+     """
+    This function takes the randomly selected word as the parameter.
+    A sequence of dashes (_) and some letters will be displayed to the user.
+    """
+    letters = []
+    temp = '_'*len(word)
+    for char in list(word):
+        if char not in letters:
+            letters.append(char)
+    character = random.choice(letters)
+    for num, char in enumerate(list(word)):
+        if char == character:
+            templist = list(temp)
+            templist[num] = char
+            temp = ''.join(templist)
+    return temp
