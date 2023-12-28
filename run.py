1a442a27895515f36ba9c6ce97d18ff9a009f6c3
Marchopkins96 <>
2023-12-28 20:19:04 +0000

    Add hangman drawing function

--- run.py
+++ run.py
@@ -76,3 +76,55 @@ def play_again():
         else:
             print("Please enter a valid choice.")
         print("\n")
+
+#The following function stores the imagery for the hangman visual
+def draw_hangman(chances):
+    if chances == 6:
+        print("________      ")
+        print("|      |      ")
+        print("|             ")
+        print("|             ")
+        print("|             ")
+        print("|             ")
+    elif chances == 5:
+        print("________      ")
+        print("|      |      ")
+        print("|      0      ")
+        print("|             ")
+        print("|             ")
+        print("|             ")
+    elif chances == 4:
+        print("________      ")
+        print("|      |      ")
+        print("|      0      ")
+        print("|     /       ")
+        print("|             ")
+        print("|             ")
+    elif chances == 3:
+        print("________      ")
+        print("|      |      ")
+        print("|      0      ")
+        print("|     /|      ")
+        print("|             ")
+        print("|             ")
+    elif chances == 2:
+        print("________      ")
+        print("|      |      ")
+        print("|      0      ")
+        print("|     /|\     ")
+        print("|             ")
+        print("|             ")
+    elif chances == 1:
+        print("________      ")
+        print("|      |      ")
+        print("|      0      ")
+        print("|     /|\     ")
+        print("|     /       ")
+        print("|             ")
+    elif chances == 0:
+        print("________      ")
+        print("|      |      ")
+        print("|      0      ")
+        print("|     /|\     ")
+        print("|     / \     ")
+        print("|             ")
